61ab7de7358a0dd24087de9cb92c8846d5c3088a
x8k <emmecerre@gmail.com>
2021-02-25 18:30:25 +0100

    first commit

---
+++ runMemFill.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+// PrintMemUsage outputs the current, total and OS memory being used. As well as the number
+// of garage collection cycles completed.
+func PrintMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
+}
+
+// RunMemFill fill RAM using the gived MB value
+func RunMemFill(quantity int) {
+	var dimension int = (quantity * 1024 * 1024) / 16
+	buffer := make([]string, dimension, dimension)
+
+	for i :=0; i < len(buffer); i++ {
+		buffer[i] = "a"
+	}
+
+	PrintMemUsage()
+	for {
+		// infitite loop
+	}
+}
+
